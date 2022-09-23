4ea75e35dca35b2c0e810bc3b5404c2e095f895f
Matias Repetto Zecchi <repetto.matias3@gmail.com>
2022-09-23 15:26:14 -0300

    Add files via upload

---
+++ script_prueba.m
@@ -0,0 +1,18 @@
+clear;
+casos_de_prueba.m
+
+mat_salidaColumnas = length(mat_casos(1,:));
+mat_salidaFilas = length(mat_casos(:,1));
+fallos = [];
+
+mat_salida = [1:mat_salidaColumnas;1:mat_salidaFilas];
+
+for i = 1:mat_salidaFilas
+  [pos, cc] = encontrearMejor(vect_ref, mat_casos(i,:));
+  if pos != -1 && cc != -1
+    mat_salida(i,:) = vect_ref(pos:mat_salidaColumnas);
+    if mat_salida(i,:) != mat_salida_ref(i,:)
+      fallos(length(fallos)+1) = i;
+    endif
+  endif
+endfor
