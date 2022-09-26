f290ec52827f6ad212d8eff460e567724659d921
Matias Repetto Zecchi <repetto.matias3@gmail.com>
2022-09-26 17:17:31 -0300

    Update script_prueba.m

--- script_prueba.m
+++ script_prueba.m
@@ -12,7 +12,7 @@ for i = 1:mat_salidaFilas
   [pos, cc] = encontrarMejor(vect_ref, mat_casos(i,:));
   if pos != -1 && cc != -1
     mat_salida(i,:) = vect_ref(pos:pos+length(mat_casos(i,:))-1);
-    if mat_salida(i,:) != mat_salida_ref(i,:)
+    if length(mat_salida_ref) != 0 && !isequal(mat_salida(i,:), mat_salida_ref(i,:))
       fallos(length(fallos)+1) = i;
     endif
   endif
